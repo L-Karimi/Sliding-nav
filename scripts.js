5156ca91f0b5ee6e342960fe6b4ff5d261586815
L-Karimi <luciahkei101@gmail.com>
2022-05-16 08:26:56 +0300

    modified index.html and scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,4 @@
-/** 
-slider
----------------------------------------------------------------------
-Author: 		Lane Olson
-Sources: 		jQuery Plugin Patterns:
-					http://coding.smashingmagazine.com/2011/10/11/essential-jquery-plugin-patterns/
-Download:     https://github.com/Lane/Slider/
-Version:		  1.0
-Description:	Displays nested lists of links as a sliding menu
----------------------------------------------------------------------
-**/
+
 ;(function ($) {
 
     // initialize namespace if it doesn't exist
